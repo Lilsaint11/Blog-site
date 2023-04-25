62e9f664a669d37b23e1428b729f1eb5de3f35c9
Stephen <saintobis11@gmail.com>
2023-04-25 09:04:08 +0100

    dropdown menu fixed

--- src/navbar.js
+++ src/navbar.js
@@ -2,11 +2,12 @@ import { Link,NavLink } from 'react-router-dom';
 import { RxHamburgerMenu } from 'react-icons/rx';
 import {AiOutlineClose} from 'react-icons/ai';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Navbar = () => {
     const [burgerClicked, setBurgerClicked] = useState(false);
     const [linksClass, setLinksClass] = useState("links")
-    const dislayMenu = () => {
+    const displayMenu = () => {
         setBurgerClicked((prevState) => !prevState);
         if(!burgerClicked) {
             setLinksClass("links active")
@@ -14,16 +15,31 @@ const Navbar = () => {
             setLinksClass("links")
         }
     }
+    useEffect(()=>{
+        setLinksClass("links")
+    },[])
     return ( 
         <nav className="navbar">
             <Link to="/" className='logo'><h1>Stephen's Blog</h1></Link>
             <div className={linksClass}>
-                <NavLink to="/" className='navlink'>Home</NavLink>
-                <NavLink to="/about" className='navlink'>About</NavLink>
-                <NavLink to="/blogs" className='navlink'>Blogs</NavLink>
-                <NavLink to="/contact" className='navlink'>Contact</NavLink>
+                <NavLink to="/" className='navlink' onClick={()=>{
+                    setLinksClass("links");
+                    setBurgerClicked(false);
+                }}>Home</NavLink>
+                <NavLink to="/about" className='navlink' onClick={()=>{
+                    setLinksClass("links");
+                    setBurgerClicked(false);
+                }}>About</NavLink>
+                <NavLink to="/blogs" className='navlink' onClick={()=>{
+                    setLinksClass("links");
+                    setBurgerClicked(false);
+                }}>Blogs</NavLink>
+                <NavLink to="/contact" className='navlink' onClick={()=>{
+                    setLinksClass("links");
+                    setBurgerClicked(false);
+                }}>Contact</NavLink>
             </div>
-            <div className='hamburger' onClick={dislayMenu}>
+            <div className='hamburger' onClick={displayMenu}>
                 {burgerClicked ? <AiOutlineClose/> : <RxHamburgerMenu />}
             </div>
         </nav>
